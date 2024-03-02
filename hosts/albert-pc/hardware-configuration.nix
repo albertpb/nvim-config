cf07b845ced0dccd346084790048c155759ad19c
albertpb <palumbo.alb@gmail.com>
2024-03-02 02:39:18 -0300

    disable data partition

--- hosts/albert-pc/hardware-configuration.nix
+++ hosts/albert-pc/hardware-configuration.nix
@@ -56,12 +56,12 @@
       fsType = "ext4";
     };
 
-  fileSystems."/data" =
-    {
-      device = "/dev/disk/by-uuid/9C4A76964A766D40";
-      fsType = "ntfs-3g";
-      options = [ "rw" "uid=1000" ];
-    };
+  #fileSystems."/data" =
+  #  {
+  #    device = "/dev/disk/by-uuid/9C4A76964A766D40";
+  #    fsType = "ntfs-3g";
+  #    options = [ "rw" "uid=1000" ];
+  #  };
 
   swapDevices =
     [{ device = "/dev/disk/by-uuid/e4abad48-6b98-4468-93b5-aacd3a72a934"; }];
