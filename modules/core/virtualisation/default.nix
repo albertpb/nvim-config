1215180afac6d19229ed70c92a1e9897faf88bbe
albertpb <palumbo.alb@gmail.com>
2024-03-02 02:39:33 -0300

    adjust huge pages

--- modules/core/virtualisation/default.nix
+++ modules/core/virtualisation/default.nix
@@ -12,14 +12,15 @@
       devices = [ "10de:2204" "10de:1aef" "10de:2684" "10de:22ba" ];
       disableEFIfb = false;
       blacklistNvidia = true;
-      # ignoreMSRs = true;
+      ignoreMSRs = true;
+      applyACSpatch = false;
     };
 
     hugepages = {
       enable = true;
-      defaultPageSize = "2M";
-      pageSize = "2M";
-      numPages = 16000;
+      defaultPageSize = "1G";
+      pageSize = "1G";
+      numPages = 32;
     };
   };
 
