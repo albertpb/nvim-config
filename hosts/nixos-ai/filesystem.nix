c135ac59dfc35b908010c9b256f3ddaf85edbec2
albertpb <palumbo.alb@gmail.com>
2024-03-02 23:32:50 -0300

    update nixos-ai filesystem

--- hosts/nixos-ai/filesystem.nix
+++ hosts/nixos-ai/filesystem.nix
@@ -5,19 +5,31 @@
 
 {
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/833bd6b9-6f23-4537-b129-58e977ffe5d8";
+    {
+      device = "/dev/disk/by-uuid/833bd6b9-6f23-4537-b129-58e977ffe5d8";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2DAE-57A1";
+    {
+      device = "/dev/disk/by-uuid/2DAE-57A1";
       fsType = "vfat";
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/8a3c9db6-e9c3-496f-9316-bdc0fa7e29eb";
+    {
+      device = "/dev/disk/by-uuid/8a3c9db6-e9c3-496f-9316-bdc0fa7e29eb";
       fsType = "ext4";
     };
 
-  swapDevices = [ ];
+  fileSystems."/ai" =
+    {
+      device = "/dev/disk/by-uuid/BA5EBD5B5EBD115D";
+      fsType = "ntfs-3g";
+      options = [ "rw" "uid=1000" ];
+    };
+
+  swapDevices = [{
+    device = "/dev/disk/by-uuid/1b050d88-785a-4870-a53b-12d82ff75408";
+  }];
 }
