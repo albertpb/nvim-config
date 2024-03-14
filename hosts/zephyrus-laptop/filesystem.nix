399a6a22e6277dd2d018a87961127d7db065bb5a
albertpb <palumbo.alb@gmail.com>
2024-03-14 00:16:17 -0300

    add filesystem zephyrus

--- hosts/zephyrus-laptop/filesystem.nix
+++ hosts/zephyrus-laptop/filesystem.nix
@@ -1,23 +1,21 @@
 { pkgs, ... }:
 {
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/03c46186-e909-466a-bc8c-6a153b1ad10a";
+    { device = "/dev/disk/by-uuid/05f4bc63-a94f-4ace-97b2-7471a9615817";
+      fsType = "ext4";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/949c3080-217b-4728-8b92-178f84ad5ba6";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/A79E-840C";
+    { device = "/dev/disk/by-uuid/DAAD-E81C";
       fsType = "vfat";
     };
 
-  fileSystems."/home" =
-    {
-      device = "/dev/disk/by-uuid/6da72a1b-4143-436c-9f29-bfb210c51c8d";
-      fsType = "ext4";
-    };
-
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/8ec170f2-b4c8-4af5-967f-29e12cb4e26c"; }];
+    [ { device = "/dev/disk/by-uuid/0e6d93fa-ffdb-40f0-954f-ee04bcef70eb"; }
+    ];
 }
