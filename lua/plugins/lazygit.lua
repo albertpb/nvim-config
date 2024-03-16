6efe3f5b91236e0d3e8539e68a0d021aa77c61ac
albertpb <palumbo.alb@gmail.com>
2024-03-16 16:46:32 -0300

    add lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,15 @@
+vim.keymap.set('n', '<leader>gg', ':LazyGit <CR>', {silent = true})
+
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
