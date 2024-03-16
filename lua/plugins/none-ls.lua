8b94338759ace268144b561e43efd181c2ff8ad6
albertpb <palumbo.alb@gmail.com>
2024-03-16 13:01:52 -0300

    fix eslint_d

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
@@ -8,10 +11,10 @@ return {
         null_ls.builtins.formatting.stylua,
         -- javascript
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
+        require("none-ls.formatting.eslint_d"),
         -- python
+        null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.isort
       },
     })
 
