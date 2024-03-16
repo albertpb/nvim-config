1932b4e5551acc655cdc3ba9c411fcf26228615d
albertpb <palumbo.alb@gmail.com>
2024-03-16 14:59:35 -0300

    add completion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,29 +1,25 @@
 return {
   {
     "VonHeikemen/lsp-zero.nvim",
-    branch = 'v3.x',
+    branch = "v3.x",
     config = function()
       local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_lspconfig()
 
       lsp_zero.set_preferences({
         call_servers = "global",
       })
 
-      lsp_zero.configure("lua_ls", {
-        settings = {
-          Lua = {
-            diagnostics = { globals = { 'vim' } },
-            runtime = { version = 'LuaJIT' },
-            telemetry = { enable = false },
-          },
-        },
-      })
-
       lsp_zero.setup_servers({
         "lua_ls",
         "nil_ls",
-        "tsserver"
+        "tsserver",
+      })
+
+      lsp_zero.set_sign_icons({
+        error = "✘",
+        warn = "▲",
+        hint = "⚑",
+        info = "»",
       })
 
       local function on_attach(_, bufnr)
@@ -32,25 +28,12 @@ return {
 
       lsp_zero.on_attach(on_attach)
 
-      lsp_zero.format_on_save({
-        format_opts = {
-          async = false,
-          timeout_ms = 10000,
-        },
-        servers = {
-          ['lua_ls'] = { 'lua' },
-          ['nil_ls'] = { 'nix' },
-        }
-      })
-
-      lsp_zero.setup()
-      vim.diagnostic.config({})
-
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.html.setup({})
       lspconfig.nil_ls.setup({})
       lspconfig.tsserver.setup({})
     end,
-    dependencies = { "neovim/nvim-lspconfig" }
+    dependencies = { "neovim/nvim-lspconfig" },
   },
 }
