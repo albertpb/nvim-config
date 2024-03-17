6764c4f4957792f02ca9a6ed36c287e42bdb36d0
albertpb <palumbo.alb@gmail.com>
2024-03-17 19:04:19 -0300

    adjust style tmux

--- modules/core/programs/tmux.nix
+++ modules/core/programs/tmux.nix
@@ -31,10 +31,13 @@
           set -g @catppuccin_window_current_fill "number"
           set -g @catppuccin_window_current_text "#{pane_current_path}"
 
-          set -g @catppuccin_status_modules_right "application session date_time"
-          set -g @catppuccin_status_left_separator  ""
-          set -g @catppuccin_status_right_separator " "
-          set -g @catppuccin_status_right_separator_inverse "yes"
+          set -g @catppuccin_window_status_enable "yes"
+          set -g @catppuccin_window_status_icon_enable "yes"
+        
+          set -g @catppuccin_status_modules_left "directory user application"
+          set -g @catppuccin_status_modules_right "session host date_time"
+          set -g @catppuccin_status_left_separator "█"
+          set -g @catppuccin_status_right_separator "█"
           set -g @catppuccin_status_fill "all"
           set -g @catppuccin_status_connect_separator "no"
       
