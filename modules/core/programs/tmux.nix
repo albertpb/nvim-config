4af227a8f33829db720f8ea0c3d23767afdbf25a
albertpb <palumbo.alb@gmail.com>
2024-03-17 00:29:14 -0300

    update theme

--- modules/core/programs/tmux.nix
+++ modules/core/programs/tmux.nix
@@ -8,7 +8,6 @@
 
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
-      better-mouse-mode
       {
         plugin = catppuccin;
         extraConfig = ''
@@ -22,6 +21,23 @@
           set -g @catppuccin_icon_window_activity "󱅫 "
           set -g @catppuccin_icon_window_bell "󰂞 "
 
+          set -g @catppuccin_window_left_separator "█"
+          set -g @catppuccin_window_right_separator "█ "
+          set -g @catppuccin_window_number_position "right"
+          set -g @catppuccin_window_middle_separator "  █"
+
+          set -g @catppuccin_window_default_fill "number"
+
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#{pane_current_path}"
+
+          set -g @catppuccin_status_modules_right "application session date_time"
+          set -g @catppuccin_status_left_separator  ""
+          set -g @catppuccin_status_right_separator " "
+          set -g @catppuccin_status_right_separator_inverse "yes"
+          set -g @catppuccin_status_fill "all"
+          set -g @catppuccin_status_connect_separator "no"
+      
         '';
       }
     ];
