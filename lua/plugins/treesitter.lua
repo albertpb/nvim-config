43b8cdb23e068702773f5e716cf3afb71e88ebbd
albertpb <palumbo.alb@gmail.com>
2024-03-18 19:01:23 -0300

    add various langs to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,22 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "nix", "javascript", "typescript", "markdown", "markdown_inline" },
+      ensure_installed = {
+        "lua",
+        "nix",
+        "javascript",
+        "typescript",
+        "tsx",
+        "sql",
+        "rust",
+        "go",
+        "python",
+        "json",
+        "html",
+        "markdown",
+        "markdown_inline",
+        "yaml",
+        "toml" },
       highlight = { enable = true },
       indent = { enable = true },
     })
