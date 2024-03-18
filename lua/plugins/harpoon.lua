e3a0a954c575117dbe4f9c995a0f23efc0513801
albertpb <palumbo.alb@gmail.com>
2024-03-17 21:33:03 -0300

    add harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,56 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():append()
+    end)
+
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local make_finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+          results = paths,
+        })
+      end
+
+      require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = make_finder(),
+            previewer = conf.file_previewer({}),
+            sorter = conf.generic_sorter({}),
+            attach_mappings = function(prompt_buffer_number, map)
+              map(
+                "i",
+                "<C-d>",
+                function()
+                  local state = require("telescope.actions.state")
+                  local selected_entry = state.get_selected_entry()
+                  local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                  harpoon:list():removeAt(selected_entry.index)
+                  current_picker:refresh(make_finder())
+                end
+              )
+
+              return true
+            end,
+          })
+          :find()
+    end
+
+    vim.keymap.set("n", "<C-e>", function()
+      toggle_telescope(harpoon:list())
+    end, { desc = "Open harpoon window" })
+  end,
+}
