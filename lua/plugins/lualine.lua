4120151c0051fd20e6afdeaf636684098612358f
albertpb <palumbo.alb@gmail.com>
2024-04-16 16:05:44 -0400

    feat: add relative path to inactive window to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,21 +11,21 @@ return {
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { {
           'filename',
-          file_status = true,       -- Displays file status (readonly status, modified status)
-          newfile_status = false,   -- Display new file status (new file means no write after created)
-          path = 1,                 -- 0: Just the filename
+          file_status = true,     -- Displays file status (readonly status, modified status)
+          newfile_status = false, -- Display new file status (new file means no write after created)
+          path = 1,               -- 0: Just the filename
           -- 1: Relative path
           -- 2: Absolute path
           -- 3: Absolute path, with tilde as the home directory
           -- 4: Filename and parent dir, with tilde as the home directory
 
-          shorting_target = 40,   -- Shortens path to leave 40 spaces in the window
+          shorting_target = 40, -- Shortens path to leave 40 spaces in the window
           -- for other components. (terrible name, any suggestions?)
           symbols = {
-            modified = '[+]',        -- Text to show when the file is modified.
-            readonly = '[-]',        -- Text to show when the file is non-modifiable or readonly.
-            unnamed = '[No Name]',   -- Text to show for unnamed buffers.
-            newfile = '[New]',       -- Text to show for newly created file before first write
+            modified = '[+]',      -- Text to show when the file is modified.
+            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+            unnamed = '[No Name]', -- Text to show for unnamed buffers.
+            newfile = '[New]',     -- Text to show for newly created file before first write
           }
         } },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
@@ -35,7 +35,10 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename' },
+        lualine_c = { {
+          'filename',
+          path = 1,
+        } },
         lualine_x = { 'location' },
         lualine_y = {},
         lualine_z = {}
