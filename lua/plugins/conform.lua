bc2f17684ca5210dd6eba0b76027111071002c0f
albertpb <palumbo.alb@gmail.com>
2024-05-19 14:13:51 -0400

    feat: add support java

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,23 +1,25 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      -- Conform will run multiple formatters sequentially
-      python = { "isort", "black" },
-      --
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
-      css = { "prettier" },
-      html = { "prettier" },
-      json = { "prettier" },
-      yaml = { "prettier" },
-      markdown = { "prettier" },
-      graphql = { "prettier" },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			python = { "isort", "black" },
+			--
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			graphql = { "prettier" },
 
-      nix = { "nixfmt" },
-    },
-  },
+			nix = { "nixfmt" },
+
+			java = { "google-java-format" },
+		},
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  dependencies = { "windwp/nvim-ts-autotag" },
-  config = function()
-    require("nvim-ts-autotag").setup()
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = { "windwp/nvim-ts-autotag" },
+	config = function()
+		require("nvim-ts-autotag").setup()
 
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {
-        "lua",
-        "nix",
-        "javascript",
-        "typescript",
-        "tsx",
-        "sql",
-        "rust",
-        "go",
-        "python",
-        "json",
-        "html",
-        "markdown",
-        "markdown_inline",
-        "yaml",
-        "toml",
-        "terraform",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"lua",
+				"nix",
+				"javascript",
+				"typescript",
+				"tsx",
+				"sql",
+				"rust",
+				"go",
+				"python",
+				"json",
+				"html",
+				"markdown",
+				"markdown_inline",
+				"yaml",
+				"toml",
+				"terraform",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
