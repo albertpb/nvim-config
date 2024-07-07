48017ad758be3ee50f0d8c8895fc6f37a13dc232
albertpb <palumbo.alb@gmail.com>
2024-07-07 00:42:19 -0400

    fix move

--- lua/plugins/move.lua
+++ lua/plugins/move.lua
@@ -4,18 +4,33 @@ return {
 		--- Config
 	},
 	config = function()
-		require("move").setup({})
+		require("move").setup({
+			line = {
+				enable = true, -- Enables line movement
+				indent = true, -- Toggles indentation
+			},
+			block = {
+				enable = true, -- Enables block movement
+				indent = true, -- Toggles indentation
+			},
+			word = {
+				enable = true, -- Enables word movement
+			},
+			char = {
+				enable = true, -- Enables char movement
+			},
+		})
 
 		local opts = { noremap = true, silent = true }
 		-- Normal-mode commands
-		vim.keymap.set("n", "<A-j>", ":MoveLine(1)<CR>", opts)
-		vim.keymap.set("n", "<A-k>", ":MoveLine(-1)<CR>", opts)
-		vim.keymap.set("n", "<A-h>", ":MoveHChar(-1)<CR>", opts)
-		vim.keymap.set("n", "<A-l>", ":MoveHChar(1)<CR>", opts)
-		vim.keymap.set("n", "<M-j>", ":MoveLine(1)<CR>", opts)
-		vim.keymap.set("n", "<M-k>", ":MoveLine(-1)<CR>", opts)
-		vim.keymap.set("n", "<M-h>", ":MoveHChar(-1)<CR>", opts)
-		vim.keymap.set("n", "<M-l>", ":MoveHChar(1)<CR>", opts)
+		vim.keymap.set("n", "<A-Down>", ":MoveLine(1)<CR>", opts)
+		vim.keymap.set("n", "<A-Up>", ":MoveLine(-1)<CR>", opts)
+		vim.keymap.set("n", "<A-Left>", ":MoveHChar(-1)<CR>", opts)
+		vim.keymap.set("n", "<A-Right>", ":MoveHChar(1)<CR>", opts)
+		vim.keymap.set("n", "<M-Down>", ":MoveLine(1)<CR>", opts)
+		vim.keymap.set("n", "<M-Up>", ":MoveLine(-1)<CR>", opts)
+		vim.keymap.set("n", "<M-Left>", ":MoveHChar(-1)<CR>", opts)
+		vim.keymap.set("n", "<M-Right>", ":MoveHChar(1)<CR>", opts)
 
 		vim.keymap.set("n", "<leader>wf", ":MoveWord(1)<CR>", opts)
 		vim.keymap.set("n", "<leader>wb", ":MoveWord(-1)<CR>", opts)
