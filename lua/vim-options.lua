d2c29d76d518c792e6ee2cd08df13a582dd58226
albertpb <palumbo.alb@gmail.com>
2025-12-12 19:28:51 -0300

    set relative numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,7 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard=unnamedplus")
 vim.cmd("set number")
 vim.cmd("set nocompatible")
+vim.cmd("set relativenumber")
 vim.cmd("filetype plugin on")
 vim.cmd("syntax on")
 vim.g.mapleader = " "
