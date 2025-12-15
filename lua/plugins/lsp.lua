e73a545ce1eaa7af9377514414da58cb349a64e5
albertpb <palumbo.alb@gmail.com>
2025-12-14 22:40:48 -0300

    fix lsp key maps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,5 +4,31 @@ return {
     vim.lsp.enable('lua_ls')
     vim.lsp.enable('nil_ls')
     vim.lsp.enable('gopls')
+
+    local map = vim.keymap.set
+    map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+    map("n", "gsd", function()
+      vim.cmd("split")
+      vim.lsp.buf.definition()
+    end, { desc = "Definition (horizontal split)" })
+
+    map("n", "gvd", function()
+      vim.cmd("vsplit")
+      vim.lsp.buf.definition()
+    end, { desc = "Definition (vertical split)" })
+
+    -- Declaration
+    map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+
+    -- Implementation
+    map("n", "gsi", function()
+      vim.cmd("split")
+      vim.lsp.buf.implementation()
+    end, { desc = "Implementation (horizontal split)" })
+
+    map("n", "gvi", function()
+      vim.cmd("vsplit")
+      vim.lsp.buf.implementation()
+    end, { desc = "Implementation (vertical split)" })
   end,
 }
